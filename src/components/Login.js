809a253ce4cc3073bd4eb200e72831f1a71cb8d3
Dmitry <113120694+TokmakDA@users.noreply.github.com>
2023-04-09 21:04:25 +0300

    fix Form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,67 +1,15 @@
-import React, { useState } from 'react';
-import { Navigate } from 'react-router-dom';
+import React from 'react';
+import FormTemplate from './FormTemplate';
 
 const Login = ({ isLoggedIn, onLogin }) => {
-  const [userData, setUserData] = useState({
-    email: '',
-    password: '',
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setUserData({
-      ...userData,
-      [name]: value,
-    });
-  };
-
-  if (isLoggedIn) {
-    return <Navigate to="/" />;
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onLogin(userData);
+  const form = {
+    name: 'login',
+    title: 'Вход',
+    button: 'Войти',
   };
 
   return (
-    <div className="">
-      <div className="spinner">
-        <i></i>
-      </div>
-      <form className="form" name="new-card-form" onSubmit={handleSubmit}>
-        <h2 className="form__title form__title_margin-bottom_m">Вход</h2>
-        <fieldset className="form__inputs">
-          <input
-            type="email"
-            className="form__input"
-            id="email"
-            placeholder="Email"
-            value={userData.email}
-            onChange={handleChange}
-            name="email"
-            required
-            maxLength="100"
-          />
-          <span id="email-error" className="form__error"></span>
-          <input
-            type="password"
-            className="form__input"
-            id="password"
-            placeholder="Пароль"
-            value={userData.password}
-            onChange={handleChange}
-            name="password"
-            maxLength="100"
-            required
-          />
-          <span id="password-error" className="form__error"></span>
-        </fieldset>
-        <button type="submit" className="form__button">
-          {'Войти'}
-        </button>
-      </form>
-    </div>
+    <FormTemplate isLoggedIn={isLoggedIn} onSubmit={onLogin} form={form} />
   );
 };
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,70 +1,20 @@
-import React, { useState } from 'react';
-import { Link, Navigate } from 'react-router-dom';
+import React from 'react';
+import FormTemplate from './FormTemplate';
+import { Link } from 'react-router-dom';
 
 const Register = ({ isLoggedIn, onRegister }) => {
-  const [userData, setUserData] = useState({
-    email: '',
-    password: '',
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setUserData({
-      ...userData,
-      [name]: value,
-    });
-  };
-
-  if (isLoggedIn) {
-    return <Navigate to="/" />;
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onRegister(userData);
+  const form = {
+    name: 'register',
+    title: 'Регистрация',
+    button: 'Зарегистрироваться',
   };
 
   return (
-    <div className="">
-      <div className="spinner">
-        <i></i>
-      </div>
-      <form className="form" name="new-card-form" onSubmit={handleSubmit}>
-        <h2 className="form__title form__title_margin-bottom_m">Регистрация</h2>
-        <fieldset className="form__inputs">
-          <input
-            type="email"
-            className="form__input"
-            id="email"
-            placeholder="Email"
-            value={userData.email}
-            onChange={handleChange}
-            name="email"
-            maxLength="100"
-            required
-          />
-          <span id="email-error" className="form__error"></span>
-          <input
-            type="password"
-            className="form__input"
-            id="password"
-            placeholder="Пароль"
-            value={userData.password}
-            onChange={handleChange}
-            name="password"
-            maxLength="100"
-            required
-          />
-          <span id="password-error" className="form__error"></span>
-        </fieldset>
-        <button type="submit" className="form__button">
-          {'Зарегистрироваться'}
-        </button>
-        <Link className="form__link" to="../sign-in">
-          Уже зарегистрированы? Войти
-        </Link>
-      </form>
-    </div>
+    <FormTemplate isLoggedIn={isLoggedIn} onSubmit={onRegister} form={form}>
+      <Link className="form__link" to="../sign-in">
+        Уже зарегистрированы? Войти
+      </Link>
+    </FormTemplate>
   );
 };
 
