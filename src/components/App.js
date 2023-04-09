038b09adeed87682f5e584909d0c5db815095fda
Dmitry <113120694+TokmakDA@users.noreply.github.com>
2023-04-09 21:34:33 +0300

    fix Form

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 
 import '../index.css';
 import Header from './Header';
