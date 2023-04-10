cd032645b257b5e0571196d8d1d87cbd373d9496
Dmitry <113120694+TokmakDA@users.noreply.github.com>
2023-04-10 20:33:20 +0300

    fix 1 iteration

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -3,16 +3,13 @@ import { useEffect } from 'react';
 const Popup = ({ isOpen, name, onClose, children }) => {
   // указываем `useEffect` для обработчика `Escape`
   useEffect(() => {
-    if (isOpen) {
-      document.addEventListener('keydown', handleEscapeKey);
-    }
+    if (!isOpen) return;
     function handleEscapeKey(e) {
       if (e.code === 'Escape') {
         onClose();
-        console.log('Escape');
       }
     }
-
+    document.addEventListener('keydown', handleEscapeKey);
     return () => document.removeEventListener('keydown', handleEscapeKey);
   }, [isOpen, onClose]);
 
@@ -28,15 +25,13 @@ const Popup = ({ isOpen, name, onClose, children }) => {
       className={`popup ${isOpen && 'popup_is-opened'} popup_type_${name}`}
       onClick={handleOverlay}
     >
-      {' '}
       <div className={`popup__container popup__container_type_${name}`}>
         <button
           className="popup__close"
           onClick={onClose}
           type="button"
         ></button>
-        {/* тут может быть любой контент попапа в `children`: хоть для попапа картинки, хоть для `InfoToolTip`, 
-        хоть для `PopupWithForm` */}
+
         {children}
       </div>
     </div>
